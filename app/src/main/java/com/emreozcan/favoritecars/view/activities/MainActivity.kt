db94d8ecd1a259bea1a3f3e45e482daf2d11fb36
Emre Ozcan <eozcan97@gmail.com>
2021-05-22 16:47:18 +0300

    Navigation and Toolbar Changed

--- app/src/main/java/com/emreozcan/favoritecars/view/activities/MainActivity.kt
+++ app/src/main/java/com/emreozcan/favoritecars/view/activities/MainActivity.kt
@@ -2,11 +2,35 @@ package com.emreozcan.favoritecars.view.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.emreozcan.favoritecars.R
+import com.emreozcan.favoritecars.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHost) as NavHostFragment
+        navController = navHostFragment.findNavController()
+
+        setupActionBarWithNavController(navController)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
